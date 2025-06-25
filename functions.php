de03503fe4ed9c56920f381ea70d3129a1fb2bbc
charanrao543 <charan.patangi543@gmail.com>
2025-06-25 18:22:51 +0530

    tried css
    commit

--- functions.php
+++ functions.php
@@ -5,4 +5,12 @@ function sample_block_theme_setup() {
   add_theme_support('editor-styles');
   add_editor_style();
 }
+
 add_action('after_setup_theme', 'sample_block_theme_setup');
+
+function theme_enqueue_styles() {
+    wp_enqueue_style('main-style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+
+
