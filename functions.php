282fa5cc2d69e3889599a22bf923c19d0483f86f
charanrao543 <charan.patangi543@gmail.com>
2025-06-26 07:48:49 +0530

    local image
    fetching images from locally and custom css

--- functions.php
+++ functions.php
@@ -10,7 +10,12 @@ add_action('after_setup_theme', 'sample_block_theme_setup');
 
 function theme_enqueue_styles() {
     wp_enqueue_style('main-style', get_stylesheet_uri());
+    // Example of adding another custom external stylesheet
+    wp_enqueue_style('custom-style', get_template_directory_uri() . '/assets/css/custom.css');
 }
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 
-
+function load_fontawesome() {
+    wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
+}
+add_action('wp_enqueue_scripts', 'load_fontawesome');
--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 get_header();
 ?>
 
-
 <main id="primary" class="site-main">
     <?php
     if (have_posts()) :
